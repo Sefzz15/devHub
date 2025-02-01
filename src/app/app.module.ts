278921c8c777c4630985b11882563e09461ae017
Sefzz15 <arsserafim98@gmail.com>
2025-02-01 19:18:09 +0200

    Refactor MultistepForm component to support responsive stepper orientation; update form structure and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule} from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { AsyncPipe } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { FirstpageComponent } from './components/firstpage/firstpage.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FirstpageComponent } from './components/firstpage/firstpage.component';
 import { CreateUserComponent } from './components/firstpage/create-user/create-user.component';
 import { UpdateUserComponent } from './components/firstpage/update-user/update-user.component';
 import { CreateCustomerComponent } from './components/firstpage/create-customer/create-customer.component';
 import { UpdateCustomerComponent } from './components/firstpage/update-customer/update-customer.component';
-import { ChatComponent } from './components/chat/chat.component';
-import { ThirdpageComponent } from './components/thirdpage/thirdpage.component';
 import { CreateProductComponent } from './components/firstpage/create-product/create-product.component';
 import { UpdateProductComponent } from './components/firstpage/update-product/update-product.component';
-import { SecondpageComponent } from './components/secondpage/secondpage.component';
 import { CreateOrderComponent } from './components/firstpage/create-order/create-order.component';
 import { UpdateOrderComponent } from './components/firstpage/update-order/update-order.component';
+import { SecondpageComponent } from './components/secondpage/secondpage.component';
+import { ThirdpageComponent } from './components/thirdpage/thirdpage.component';
+import { ChatComponent } from './components/chat/chat.component';
 import { MultistepformComponent } from './components/multistepform/multistepform.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -48,7 +52,6 @@ import { MatInputModule } from '@angular/material/input';
     SecondpageComponent,
     CreateOrderComponent,
     UpdateOrderComponent,
-    UpdateOrderComponent,
     MultistepformComponent,
   ],
   imports: [
@@ -58,13 +61,14 @@ import { MatInputModule } from '@angular/material/input';
     FormsModule,
     HttpClientModule,
     MatToolbarModule,
-    BrowserModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
     MatStepperModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    AsyncPipe,
+    MatIconModule,
   ],
   providers: [
     provideClientHydration(withEventReplay()),
--- src/app/components/multistepform/multistepform.component.ts
+++ src/app/components/multistepform/multistepform.component.ts
@@ -1,15 +1,22 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { StepperOrientation, MatStepperModule } from '@angular/material/stepper';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
-  selector: 'app-multistepform',
+  selector: 'stepper-responsive-example',
   standalone: false,
 
   templateUrl: './multistepform.component.html',
   styleUrls: [
     '../firstpage/firstpage.component.css',
-    './multistepform.component.css'
+    './multistepform.component.css',
   ],
 
 })
@@ -22,5 +29,16 @@ export class MultistepformComponent {
   secondFormGroup = this._formBuilder.group({
     secondCtrl: ['', Validators.required],
   });
-  isLinear = false;
+  thirdFormGroup = this._formBuilder.group({
+    thirdCtrl: ['', Validators.required],
+  });
+  stepperOrientation: Observable<StepperOrientation>;
+
+  constructor() {
+    const breakpointObserver = inject(BreakpointObserver);
+
+    this.stepperOrientation = breakpointObserver
+      .observe('(min-width: 800px)')
+      .pipe(map(({ matches }) => (matches ? 'horizontal' : 'vertical')));
+  }
 }
