0f8dd82bc191281c8ffc4190cb2507ebfa71ec06
sefzz.ars <57561143+Sefzz15@users.noreply.github.com>
2025-09-18 10:36:25 +0300

    Save current changes before Angular update

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-import { provideServerRoutesConfig } from '@angular/ssr';
+import { withRoutes, provideServerRendering } from '@angular/ssr';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 import { serverRoutes } from './app.routes.server';
 
 @NgModule({
   imports: [AppModule, ServerModule],
-  providers: [provideServerRoutesConfig(serverRoutes)],
+  providers: [
+    provideServerRendering(withRoutes(serverRoutes)),
+  ],
   bootstrap: [AppComponent],
 })
-export class AppServerModule { }
+export class AppServerModule {}
